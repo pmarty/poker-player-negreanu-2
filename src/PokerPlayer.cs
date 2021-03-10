9bf5ff1eaaa7775ecc0ae4c9d07eb170ca0fa54b
Alexander Minsch <alexander.minsch@digitecgalaxus.ch>
2021-03-10 10:48:41 +0100

    HasPair

--- src/PokerPlayer.cs
+++ src/PokerPlayer.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Linq;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace Nancy.Simple
 {
@@ -17,6 +16,12 @@ namespace Nancy.Simple
             if (HasPair(ownPlayer))
             {
                 Console.WriteLine("we have a pair");
+                return ownPlayer.stack;
+            }
+
+            if (IsSuited(ownPlayer))
+            {
+                Console.WriteLine("we have suited cards");
                 return ownPlayer.stack;
             }
 
@@ -38,6 +43,13 @@ namespace Nancy.Simple
             var cardRank = player.hole_cards.First().rank;
 
             return player.hole_cards.All(c => c.rank == cardRank);
+        }
+        
+        private static bool IsSuited(Player player)
+        {
+            var suit = player.hole_cards.First().suit;
+
+            return player.hole_cards.All(c => c.suit == suit);
         }
     }
 }
